9d5beadc7876870ce540f1a2f48641e9b8efc128
Caio Rodrigues <caio.rodrigueess@alu.ufc.br>
2024-12-15 17:31:50 +0000

    atualização exercicio 1

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,25 +1,22 @@
 def pr(pt, k, d, n):
     return pt*k/d**n
 
-def pn(k0, bt, N):
-    return k0*bt/N    
-
 def main():
     pt=1
     k=10e-4
     n=4
     N=1
-    dist = [50, 150, 110, 90]
+    d11 = 50
+    d12 = 110
+    d21 = 150
+    d22 = 90
     bt=100*10e6
     k0=10e-17
     soma=0
-
-    for i in dist:
-        soma += pr(pt, k, i, n)
     
-    UE1=pr(pt, k, dist[0], n)/(soma)
-    UE2=pr(pt, k, dist[3], n)/(soma)
-    print(f'UE1: {UE1} \nUE2: {UE2}')
+    UE1=pr(pt, k, d11, n)/pr(pt, k, d12, n)
+    UE2=pr(pt, k, d22, n)/pr(pt, k, d21, n)
+    print(f'UE1: {UE1:.02f} \nUE2: {UE2:.02f}')
 
 if __name__=='__main__':
     main()
