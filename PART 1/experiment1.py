0889b76a419d64dd3f1d3a6696281269e60d15ee
Caio Rodrigues <caio.rodrigueess@alu.ufc.br>
2025-02-08 19:56:13 -0300

    Update experiment1.py

--- PART 1/experiment1.py
+++ PART 1/experiment1.py
@@ -1,114 +1,115 @@
 import numpy as np
-import random
 import matplotlib.pyplot as plt
 
-def distribuir_APs(M):
-    if M not in [1, 4, 9, 16, 25, 36, 49, 64]:
-        return None
+class AP:
+    def __init__(self, x: float, y: float, id: int):
+        self.x = x
+        self.y = y
+        self.id = id
+        self.channel = None
 
-    tamanho_quadrado = 1000
-    lado_quadrado = int(np.sqrt(M))
+    def __str__(self):
+        return f'AP[{self.id}]({self.x}, {self.y})'
 
-    tamanho_celula = tamanho_quadrado // lado_quadrado
+class UE:
+    def __init__(self, N: int):
+        self.x = np.random.randint(0, 1001)
+        self.y = np.random.randint(0, 1001)
+        self.ap = None
+        self.channel = np.random.randint(1, N+1)
 
-    # Criar coordenadas usando meshgrid
-    x, y = np.meshgrid(np.arange(0.5 * tamanho_celula, tamanho_quadrado, tamanho_celula),
-                      np.arange(0.5 * tamanho_celula, tamanho_quadrado, tamanho_celula))
+    def __str__(self):
+        return f'UE({self.x}, {self.y})'
 
-    coordenadas_APs = np.column_stack((x.ravel(), y.ravel()))
+def distribuir_AP(M: int) -> np.ndarray:
+    APs = []
+    dx = 1000/(2*np.sqrt(M))                    # passo
+    a = np.arange(dx, 1001-dx, 2*dx)
+    x, y = np.meshgrid(a, a)
+    id = 1
+    for xi, yi in zip(x.ravel(), y.ravel()):
+        APs.append(AP(xi, yi, id))
+        id += 1
+    return APs
 
-    return coordenadas_APs
+def dist_AP_UE(M: int, N: int) -> float:
+    aps = distribuir_AP(M)
+    ue = UE(N)
+    dist = 1e4
+    for ap in aps:
+        x = np.linalg.norm(np.array([ue.x, ue.y]) - np.array([ap.x, ap.y]))
+        if x < dist:
+            dist = x
+            ue.ap = ap
+            ap.channel = ue.channel
+    # Adicionar SINR
+    # Devo calcular o SINR ou a capacidade do canal?
+    return dist
 
-def dAPUE(x_coord, y_coord, M):
-    dAPUE = np.linalg.norm(np.array([x_coord, y_coord]) - M)
-    return dAPUE
+def SINR(dist: float, N: int) -> float:
+    pt=1        # transmited power
+    k=1e-4      # constant
+    n=4         # path gain
+    bt=1e8      # avaiable bandwidth
+    k0=1e-20    # constant for the noise power
 
-def pot_rec(pot_trans, dist, d_0):
-    c = 1e-4
-    n = 4
-    pot_rec_result = 0  # Inicializa a variável local
+    pr = pt*k/(dist**n)
+    pn = k0*bt/N
+    return pr/pn
 
-    if dist >= d_0:
-        pot_rec_result = pot_trans * (c / ((dist) ** n))
+def channel_capacity(SINR: float, N: int) -> float:
+    bt=1e8      # avaiable bandwidth
+    return (bt/N)*np.log2(1+SINR)
 
-    return pot_rec_result
+def simular_experimento(M: int, N: int):
+    dist = dist_AP_UE(M, N)
+    sinr = SINR(dist, N)
+    return channel_capacity(sinr, N)
 
-def simular_experimento(B_t, p_t, d_0, K_0, M, N):
-    x_coord = random.random() * 1000
-    y_coord = random.random() * 1000
-
-    distanciaAP_UE = np.zeros(M)
-    coordAp = distribuir_APs(M)
-    potencia_recebida = np.zeros(M)
-    p_n = K_0*(B_t/N)
-    SNR = np.zeros(M)
-    for i in range(M):
-        distanciaAP_UE[i] = dAPUE(x_coord, y_coord, coordAp[i])
-        distan = distanciaAP_UE[i]
-        potencia_recebida[i] = pot_rec(p_t, distan, d_0)
-        SNR[i] = potencia_recebida[i]/p_n
-
-    #Calculando a Capacidade
-    B_c = B_t / N
-    snr_2 = np.sum(SNR)
-    Capacidade = np.zeros(K)
-    for i in range(K):
-      Capacidade[i] = B_c * np.log2(1+snr_2)
-
-    return SNR
-
-B_t, p_t, d_0, K_0 = 100e6, 1e3, 1, 1e-17 # Em MHz, mW, metros, mW/Hz respectivamente
-M_1, K, N = 1, 1, 1 #Número de APs, UEs e Canais respectivamente
-M_9, M_25, M_64 = 9, 25, 64
-
-# Número de iterações
-num_iteracoes = 1000
-
-# Armazenar todas as capacidades
-Capacidade_total_1 = []
-Capacidade_total_9 = []
-Capacidade_total_25 = []
-Capacidade_total_64 = []
-
-# Iteração
-for _ in range(num_iteracoes):
-    Capacidade_iteracao_1 = simular_experimento(B_t, p_t, d_0, K_0, M_1, N)
-    Capacidade_total_1 = np.concatenate((Capacidade_total_1, Capacidade_iteracao_1))
-
-for _ in range(num_iteracoes):
-    Capacidade_iteracao_9 = simular_experimento(B_t, p_t, d_0, K_0, M_9, N)
-    Capacidade_total_9 = np.concatenate((Capacidade_total_9, Capacidade_iteracao_9))
-
-for _ in range(num_iteracoes):
-    Capacidade_iteracao_25 = simular_experimento(B_t, p_t, d_0, K_0, M_25, N)
-    Capacidade_total_25 = np.concatenate((Capacidade_total_25, Capacidade_iteracao_25))
-
-for _ in range(num_iteracoes):
-    Capacidade_iteracao_64 = simular_experimento(B_t, p_t, d_0, K_0, M_64, N)
-    Capacidade_total_64 = np.concatenate((Capacidade_total_64, Capacidade_iteracao_64))
-
-#Deixando em ordem crescente
-x1 = np.sort(Capacidade_total_1)
-x2 = np.sort(Capacidade_total_9)
-x3 = np.sort(Capacidade_total_25)
-x4 = np.sort(Capacidade_total_64)
-
-# Plotar apenas o eixo x em decibéis
-
-plt.plot(x1, np.arange(0, len(Capacidade_total_1)) / len(Capacidade_total_1), label = '1 AP')
-plt.plot(x2, np.arange(0, len(Capacidade_total_1)) / len(Capacidade_total_1), label = '9 APs')
-plt.plot(x3, np.arange(0, len(Capacidade_total_1)) / len(Capacidade_total_1), label = '25 APs')
-plt.plot(x4, np.arange(0, len(Capacidade_total_1)) / len(Capacidade_total_1), label = '64 APs')
-plt.legend()
-
-# Adicionando linha tracejada em x=100e6
-plt.axvline(x=100e6, color='grey', linestyle='--', label='x=100e6')
-
-# Adicionando linha tracejada em y=0.1
-plt.axhline(y=0.1, color='grey', linestyle='--', label='y=0.1')
+if __name__ == '__main__':
+    M = [1, 9, 36, 64]
+    N = [1, 2, 3]
+    sim = 1000
+    '''
+    for m in M:
+        print(m)
+        aps = distribuir_AP(m)
+        for ap in aps:    
+            print(ap)
+    '''
+    cdf = np.zeros([len(M), len(N)])
+    i=0
+    for m in M:
+        j=0
+        for n in N:
+            for _ in range(sim):
+                x = simular_experimento(m, n)
+                cdf[i][j] += x
+            j+=1
+        i+=1
 
 
-plt.xlabel('Capacidade (bps)')
-plt.ylabel('Porcentagem')
-plt.title('CDF da Capacidade')
-plt.show()
+    '''
+    Até o momento, tenho uma matriz de M linhas e N colunas
+    Falta separar o vetor e plotar o gráfico
+    '''
+    cdf.sort()
+
+
+    # Código inicial do gráfico -> precisa de alterações
+    print(f'10th: {round(np.percentile(cdf, 10), 4)}')
+    print(f'50th: {round(np.percentile(cdf, 50), 4)}')
+    print(f'90th: {round(np.percentile(cdf, 90), 4)}')
+
+    # Plotar o gráfico das estimativas de área
+    plt.figure(figsize=(10, 6))
+    plt.plot(range(1, 100 + 1), cdf, label='CDF')
+    plt.axhline(y=np.percentile(cdf, 50), color='r', linestyle='--', label='Mediana')
+
+    # Configurar o gráfico
+    plt.title('Estimativa da Capacidade do Canal pelo número de simulações (Método Monte Carlo)')
+    plt.xlabel('Número de Simulações')
+    plt.ylabel('Capacidade do Canal')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
